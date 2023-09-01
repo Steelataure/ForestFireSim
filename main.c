c13228aaca1fc359759531cc857ff0269c4e6fe5
Steel <alexandre0312@orange.fr>
2023-09-01 14:34:13 +0200

    aleatoire/manuel

--- main.c
+++ main.c
@@ -12,6 +12,8 @@ struct TailleMatrice demanderTailleMatrice() {
     return taille;
 }
 
+
+
 void initialiserForetAleatoirement(struct CelluleForet foret[][100], struct TailleMatrice taille) {
     srand(time(NULL));
 
@@ -20,7 +22,6 @@ void initialiserForetAleatoirement(struct CelluleForet foret[][100], struct Tail
             char typesPossibles[] = {'+', '*', ' ', '#', 'x', '/', '-', '@'};
             int indexType = rand() % 8;
             foret[i][j].type = typesPossibles[indexType];
-
             foret[i][j].etat = 0;
             foret[i][j].degre = 0;
         }
@@ -36,12 +37,35 @@ void afficherMatrice(struct CelluleForet foret[][100], struct TailleMatrice tail
     }
 }
 
-int main() {
-    struct TailleMatrice tailleMatrice = demanderTailleMatrice();
+void typeManuel(struct CelluleForet foret[][100], struct TailleMatrice taille) {
 
+    for (int i = 0; i < taille.longueur; i++) {
+        for (int j = 0; j < taille.largeur; j++) {
+            printf("Entrez le type de la cellule [%d][%d] (A pour arbre, F pour feuille, R pour roche, etc.) : ", i, j);
+            scanf(" %c", &foret[i][j].type);
+
+            foret[i][j].etat = 0;
+            foret[i][j].degre = 0;
+        }
+    }
+}
+
+int main() {
+    int automatique_or_manuel;
+
+    struct TailleMatrice tailleMatrice = demanderTailleMatrice();
     struct CelluleForet foret[100][100];
 
-    initialiserForetAleatoirement(foret, tailleMatrice);
+    printf("Veuillez choisir entre une saisi manuel ou automatique (0 => manuel | 1 => automatique) : ");
+    scanf("%d", &automatique_or_manuel);
+
+    if (automatique_or_manuel == 0){
+        typeManuel(foret, tailleMatrice);
+    }
+    else if (automatique_or_manuel == 1){
+        initialiserForetAleatoirement(foret, tailleMatrice);
+
+    }
 
     afficherMatrice(foret, tailleMatrice);
 
